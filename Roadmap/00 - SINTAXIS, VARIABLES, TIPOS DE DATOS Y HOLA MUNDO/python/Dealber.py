584d97f6bd94794eb1b3d05883bdea1277d7a8e7
Dealber <160842351+Dealber@users.noreply.github.com>
2024-03-07 12:42:36 +0100

    Create Dealber.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Dealber.py
@@ -0,0 +1,19 @@
+# https://es.python.org/
+
+# "Comentario en una línea"
+
+"""
+Comentario
+en varias 
+lineas
+"""
+
+my_variable = "Esto es una variable"
+MYCONSTANT = "Variable constante poner en mayúsculas"
+
+my_int = 1
+my_float = 5.3
+my_bool = True or False
+my_string = "Cadena de texto"
+
+print ("Hola Python")
